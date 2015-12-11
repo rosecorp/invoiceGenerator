f6e171f40c75d7adeb403f0c0ffa83cfa95c78aa
sonorati <s.onorati@gmail.com>
2015-12-11 00:45:58 +0000

    modified main to load configuration files from external folder

--- src/main/scala/com/seon/invoicer/GeneratePdf.scala
+++ src/main/scala/com/seon/invoicer/GeneratePdf.scala
@@ -1,34 +1,39 @@
 package com.seon.invoicer
 
-import java.util.Properties
+import java.nio.file.Files.newInputStream
+import java.nio.file.Paths
 
 import com.seon.invoicer.model.{Company, CompanyBasic}
 import com.seon.invoicer.services.InvoiceService
 import com.typesafe.config.{Config, ConfigFactory}
 import play.api.libs.json.Json
 
+import scala.io.Source._
+
 object GeneratePdf {
+
+  val externalPath = "/Users/sebastianonorati/Documents/invoicer/details/"
 
   def main(args: Array[String]) {
     val daysWorked = args(0).toInt
     val invoiceNumber = args(1).toInt
 
-    val payee = Json.parse(getClass.getClassLoader.getResourceAsStream("payee.json")).as[Company]
-    val payer = Json.parse(getClass.getClassLoader.getResourceAsStream("payer.json")).as[CompanyBasic]
+    val payee = Json.parse(newInputStream(Paths.get(externalPath+"payee.json"))).as[Company]
+    val payer = Json.parse(newInputStream(Paths.get(externalPath+"payer.json"))).as[CompanyBasic]
 
     val service:InvoiceService = new InvoiceService(makePdfName(invoiceNumber), payee, payer)
     service.generatePDF(daysWorked, invoiceNumber)
   }
 
+  def conifg: Config = {
+    System.out.print(s"External config location: $externalPath")
+    ConfigFactory.parseString(fromFile(externalPath+"pdf-details.properties").mkString)
+  }
+
   def makePdfName(invoiceNumber:Int):String = {
-    val prefix = config.getString("pdf.name.prefix")
-    val suffix = config.getString("pdf.name.suffix")
+    val prefix = conifg.getString("pdf.name.prefix")
+    val suffix = conifg.getString("pdf.name.suffix")
     s"$prefix $invoiceNumber$suffix"
   }
 
-  def config: Config = {
-    val props = new Properties()
-    props.load(getClass.getClassLoader.getResourceAsStream("pdf-details.properties"))
-    ConfigFactory.parseProperties(props)
-  }
 }
